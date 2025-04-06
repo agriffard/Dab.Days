08402c4b9e06a202b1de9c916f213e177d91728c
Antoine Griffard <agriffard@hotmail.com>
2025-04-06 15:05:37 +0200

    Half days

--- Dab.Days.Tests/WorkingDaysCalculatorTests.cs
+++ Dab.Days.Tests/WorkingDaysCalculatorTests.cs
@@ -18,16 +18,26 @@ public class WorkingDaysCalculatorTests
     #region GetWorkingDaysBetweenDates
 
     [Theory]
-    //[InlineData(2023, 5, 1, 2023, 5, 31, 20)]
-    //[InlineData(2023, 6, 1, 2023, 6, 30, 22)]
-    //[InlineData(2023, 7, 1, 2023, 7, 31, 20)]
-    [InlineData(2024, 12, 22, 2025, 1, 2, 7)]
-    [InlineData(2025, 5, 1, 2025, 5, 31, 19)]
-    public void GetWorkingDaysBetweenDates_ShouldReturnCorrectCount(int startYear, int startMonth, int startDay, int endYear, int endMonth, int endDay, int expectedWorkingDays)
+    //[InlineData(2023, 5, 1, 0, 2023, 5, 31, 0, 20)]
+    //[InlineData(2023, 6, 1, 0, 2023, 6, 30, 0, 22)]
+    //[InlineData(2023, 7, 1, 0, 2023, 7, 31, 0, 20)]
+    //[InlineData(2024, 12, 22, 0, 2025, 1, 2, 0, 7)]
+    //[InlineData(2024, 12, 22, 12, 2025, 1, 2, 12, 6)]
+    //[InlineData(2024, 12, 22, 12, 2025, 1, 2, 0, 6.5)]
+    //[InlineData(2024, 12, 22, 0, 2025, 1, 2, 12, 6.5)]
+    //[InlineData(2025, 5, 1, 0, 2025, 5, 31, 0, 19)]
+    //[InlineData(2025, 4, 7, 0, 2025, 4, 7, 0, 1)] // 1 jour
+    //[InlineData(2025, 4, 7, 0, 2025, 4, 7, 12, 0.5)] // 1 matin
+    [InlineData(2025, 4, 7, 12, 2025, 4, 7, 0, 0.5)] // 1 après-midi
+    //[InlineData(2025, 4, 7, 12, 2025, 4, 8, 12, 1)] // 1 après-midi et 1 matin
+    //[InlineData(2025, 4, 7, 12, 2025, 4, 8, 0, 1.5)] // 1 après-midi et 1 jour
+    //[InlineData(2025, 4, 7, 0, 2025, 4, 8, 12, 1.5)] // 1 jour et 1 matin
+    //[InlineData(2025, 4, 7, 0, 2025, 4, 8, 0, 2)] // 2 jours
+    public void GetWorkingDaysBetweenDates_ShouldReturnCorrectCount(int startYear, int startMonth, int startDay, int startHours, int endYear, int endMonth, int endDay, int endHours, double expectedWorkingDays)
     {
         // Arrange
-        var startDate = new DateTime(startYear, startMonth, startDay);
-        var endDate = new DateTime(endYear, endMonth, endDay);
+        var startDate = new DateTime(startYear, startMonth, startDay, startHours, 0, 0);
+        var endDate = new DateTime(endYear, endMonth, endDay, endHours, 0, 0);
         //var publicHolidays = new List<DateTime> { new DateTime(2023, 5, 1) };
         //_publicHolidaysCalculatorMock.Setup(x => x.GetPublicHolidays(2023)).Returns(publicHolidays);
 
--- Dab.Days/WorkingDaysCalculator.cs
+++ Dab.Days/WorkingDaysCalculator.cs
@@ -36,23 +36,39 @@ public class WorkingDaysCalculator : IWorkingDaysCalculator
         return workingDays;
     }
 
-    public int GetWorkingDaysBetweenDates(DateTime startDate, DateTime endDate, bool workingOnSaturday = false)
+    public double GetWorkingDaysBetweenDates(DateTime startDate, DateTime endDate, bool workingOnSaturday = false)
     {
-        var workingDays = 0;
+        var workingDays = 0d;
         var currentDay = startDate;
         var isSameYear = startDate.Year == endDate.Year;
         var publicHolidays = _publicHolidaysCalculator.GetPublicHolidays(startDate.Year);
         if (!isSameYear)
             publicHolidays = publicHolidays.Concat(_publicHolidaysCalculator.GetPublicHolidays(endDate.Year));
-        while (currentDay <= endDate)
+
+        var isStartHalfDay = startDate.Hour == 12;
+        var isEndHalfDay = endDate.Hour == 12;
+
+        while (currentDay.Date <= endDate.Date)
         {
             //if neither sunday, neither saturday (not working), neither public holiday
             if (currentDay.DayOfWeek != DayOfWeek.Sunday
-                && currentDay.DayOfWeek != DayOfWeek.Saturday
-                && !workingOnSaturday
+                && (currentDay.DayOfWeek != DayOfWeek.Saturday || workingOnSaturday)
                 && !publicHolidays.Any(h => h.Year == currentDay.Year && h.Month == currentDay.Month && h.Day == currentDay.Day))
+            {
                 workingDays++;
+            }
             currentDay = currentDay.AddDays(1);
+        }
+
+        var isSameDate = startDate.Date == endDate.Date;
+        if (isStartHalfDay)
+        {
+            workingDays -= 0.5;
+        }
+
+        if (isEndHalfDay)
+        {
+            workingDays -= 0.5;
         }
 
         return workingDays;
