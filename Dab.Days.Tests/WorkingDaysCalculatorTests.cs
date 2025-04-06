ba2ed774f6e730cbc60caf4990a9987ec1aae28d
Antoine Griffard <agriffard@hotmail.com>
2025-04-06 16:33:08 +0200

    Remove FluentAssertions

--- Dab.Days.Tests/WorkingDaysCalculatorTests.cs
+++ Dab.Days.Tests/WorkingDaysCalculatorTests.cs
@@ -58,7 +58,7 @@ public class WorkingDaysCalculatorTests
     {
         var calculator = new WorkingDaysCalculator(new FrenchPublicHolidaysCalculator());
         var workingDays = calculator.GetWorkingDaysByMonth(year, month);
-        workingDays.Should().Be(expectedWorkingDays); // 20 jours travaillés en juillet 2023
+        Assert.Equal(expectedWorkingDays, workingDays); // 20 jours travaillés en juillet 2023
     }
 
     #endregion
@@ -69,21 +69,21 @@ public class WorkingDaysCalculatorTests
     public void CheckDayOffSunday()
     {
         var calculator = new WorkingDaysCalculator(new FrenchPublicHolidaysCalculator());
-        calculator.IsDayOff(new DateTime(2023, 07, 30)).Should().BeTrue(); // dimanche 30 juillet 2023
+        Assert.True(calculator.IsDayOff(new DateTime(2023, 07, 30))); // dimanche 30 juillet 2023
     }
 
     [Fact]
     public void CheckDayOffSaturday()
     {
         var calculator = new WorkingDaysCalculator(new FrenchPublicHolidaysCalculator());
-        calculator.IsDayOff(new DateTime(2023, 07, 29)).Should().BeTrue(); // samedi 29 juillet 2023
+        Assert.True(calculator.IsDayOff(new DateTime(2023, 07, 29))); // samedi 29 juillet 2023
     }
 
     [Fact]
     public void CheckDayOffHoliday()
     {
         var calculator = new WorkingDaysCalculator(new FrenchPublicHolidaysCalculator());
-        calculator.IsDayOff(new DateTime(2023, 05, 08)).Should().BeTrue(); // 8 Mai ferié
+        Assert.True(calculator.IsDayOff(new DateTime(2023, 05, 08))); // 8 Mai férié
     }
 
     #endregion
